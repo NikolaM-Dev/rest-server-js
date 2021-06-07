bb3b6df5f599cc09364934086b1fe9cba378ee00
David Merchan [NikolaM-Dev] <juan1014166@gmail.com>
2021-06-07 15:43:16 -0500

    post user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,7 @@
 const { response, request } = require('express');
 
+const User = require('../models/user');
+
 const getUsers = (req = request, res = response) => {
   const { name = 'No name', apiKey, page = 1, limit = 10 } = req.query;
 
@@ -10,8 +12,10 @@ const getUser = (_req = request, res = response) => {
   res.json({ msg: 'get API - Controller' });
 };
 
-const postUser = (req = request, res = response) => {
-  const user = req.body;
+const postUser = async (req = request, res = response) => {
+  const body = req.body;
+  const user = new User(body);
+  await user.save();
 
   res.status(201).json({ user });
 };
