e3787ba4726261732e167f3c7fe855f058eb2629
David Merchan <juan1014166@gmail.com>
2021-06-15 10:25:00 -0500

    category model

---
+++ src/models/category.model.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require('mongoose');
+
+const CategorySchema = Schema({
+  name: { type: String, required: [true, 'Name is required'] },
+  state: { type: Boolean, required: true, default: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+module.exports = model('Category', CategorySchema);
---
+++ src/models/index.js
@@ -0,0 +1,11 @@
+const Category = require('./category.model');
+const Role = require('./role.model');
+const User = require('./user.model');
+const Server = require('./server.model');
+
+module.exports = {
+  Category,
+  Role,
+  User,
+  Server,
+};
